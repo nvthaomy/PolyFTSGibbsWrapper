9ccf1a6371959fd2d4bb9b6cff5ea3a5c466218b
My Nguyen <mnguyen@braid.cnsi.ucsb.edu>
2020-06-21 17:28:30 -0700

    print out values for testing

--- Gibbs_V4.py
+++ Gibbs_V4.py
@@ -154,7 +154,7 @@ class Gibbs_System():
         '''Check for electroneutrality'''
         for Box in [1,2]:
             q = self.GetTotalCharge(Box)
-            self.Write2Log('\nTotal charge in box {}: {}'.format(Box,q))
+            self.Write2Log('Total charge in box {}: {}\n'.format(Box,q))
     
     def CheckTotalSpeciesC(self, ConcTol = 0.01):
         for i in range(self.Nspecies):
@@ -861,7 +861,7 @@ class Gibbs_System():
                             # update neutral species first   
                             if indx - 1 in self.NeutralSpecies: 
                                 self.Write2Log('==Updating neutral species {}==\n'.format(indx - 1))
-                                self.Write2Log('muI - mu II = {} - {} = {}\n'.format(self.OperatorsCurrent[8+4*(indx-1)],self.OperatorsCurrent[8+4*(indx-1)+2],DvalsCurrent[indx+1]))
+                                self.Write2Log('muI - mu II = {} - {} = {}\n'.format(self.OperatorsCurrent[8+4*(indx-1)],self.OperatorsCurrent[8+4*(indx-1)+2],self.DvalsCurrent[indx+1]))
                                 self.Write2Log('current conc of species in box I {}\n'.format(var))
                                 if self.UseAdaptiveDtC:  
                                     Dt_tmp = self.Dt[indx]
@@ -877,14 +877,14 @@ class Gibbs_System():
 
                     # update charged pair concentration
                     for indx, [i,j] in enumerate(self.ChargedPairs):
-                        self.Write2Log('==Updating charged pair {} = {}(charge {}) + {}(charge {})==\n'.format(indx,i,j,self.Charges[i],self.Charges[j]))
+                        self.Write2Log('==Updating charged pair {} = {}(charge {}) + {}(charge {})==\n'.format(indx,i,self.Charges[j],j,self.Charges[i]))
                         self.Write2Log('muI_i {}, muII_i {}\n'.format(self.OperatorsCurrent[8+4*i],self.OperatorsCurrent[8+4*i+2]))
                         self.Write2Log('muI_j {}, muII_j {}\n'.format(self.OperatorsCurrent[8+4*j],self.OperatorsCurrent[8+4*j+2]))
                         self.Write2Log('muI = {} * {} + {} * {} = {}\n'.format(self.OperatorsCurrent[8+4*i],self.Valencies[j],self.OperatorsCurrent[8+4*j],self.Valencies[i],self.MuPair1[indx]))
                         self.Write2Log('muII = {} * {} + {} * {} = {}\n'.format(self.OperatorsCurrent[8+4*i+2],self.Valencies[j],self.OperatorsCurrent[8+4*j+2],self.Valencies[i],self.MuPair2[indx]))
-                        self.Write2Log('muI - mu II = {} - {} = {}\n'.format(self.MuPair1[indx],self.MuPair2[indx]))
+                        self.Write2Log('muI - mu II = {} - {} = {}\n'.format(self.MuPair1[indx],self.MuPair2[indx], self.MuPair1[indx] - self.MuPair2[indx]))
                         self.Write2Log('CI_i {}, CI_j {}\n'.format(self.ValuesCurrent[(i+1)*2], self.ValuesCurrent[(j+1)*2]))
-                        self.Write2Log('current conc of pair in box I (must be min(zj*Ci)) {}\n'.format(Cpair1))
+                        self.Write2Log('current conc of pair in box I (must be min(zj*Ci)) {}\n'.format(self.Cpair1[indx]))
                         Cpair1 = self.Cpair1[indx]
                         Cpair2 = self.Cpair2[indx]
                         if self.UseAdaptiveDtC:
